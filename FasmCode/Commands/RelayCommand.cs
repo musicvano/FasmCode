e2cd5d432b69ff8aff97fd818a32f9a052473b0e
Ivan Muzyka <musicvano@gmail.com>
2018-02-04 03:09:01 +0200

    Add commands

---
+++ FasmCode/Commands/RelayCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FasmCode.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute) :
+            this(execute, null)
+        { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+        private readonly Predicate<T> canExecute;
+
+        public RelayCommand(Action<T> execute) :
+            this(execute, null)
+        { }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+    }
+}
---
+++ FasmCode/ViewModels/MainViewModel.cs
@@ -0,0 +1,100 @@
+﻿using FasmCode.Commands;
+using System;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace FasmCode.ViewModels
+{
+    class MainViewModel
+    {
+        public ICommand NewCommand { get; set; }
+        public ICommand OpenCommand { get; set; }
+        public ICommand OpenFolderCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public ICommand SaveAsCommand { get; set; }
+        public ICommand SaveAllCommand { get; set; }
+        public ICommand CloseCommand { get; set; }
+        public ICommand CloseFolderCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
+
+        public ICommand CompileCommand { get; set; }
+        public ICommand RunCommand { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public MainViewModel()
+        {
+            NewCommand = new RelayCommand(New);
+            OpenCommand = new RelayCommand(Open);
+            OpenFolderCommand = new RelayCommand(OpenFolder);
+            SaveCommand = new RelayCommand(Save);
+            SaveAsCommand = new RelayCommand(SaveAs);
+            SaveAllCommand = new RelayCommand(SaveAll);
+            CloseCommand = new RelayCommand(Close);
+            CloseFolderCommand = new RelayCommand(CloseFolder);
+            ExitCommand = new RelayCommand(Exit);
+            CompileCommand = new RelayCommand(Compile);
+            RunCommand = new RelayCommand(Run);
+            Date = DateTime.Now;
+        }
+
+        public void New()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Open()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void OpenFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveAs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloseFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Exit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Compile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
